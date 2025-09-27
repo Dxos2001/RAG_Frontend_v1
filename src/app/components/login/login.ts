dda10ab544fc2de7f1d7caa4a4dc13c8880e6c71
DanGeLo24 <dangelocarrreno@gmail.com>
2025-09-26 23:05:12 -0500

    actualizado

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl:'./login.html',
-  styleUrl: './login.css'
-})
-export class Login {
-  email: string = '';
-  password: string ='';
-  mensaje: string = '';
-  mensajeColor: string = 'red';
+  templateUrl: './login.html',
+  styleUrls: ['./login.css'],
+
   
+})
+export class Login{
+  email: string = '';
+  password: string = '';
+  mensaje: string = '';
+  imagen = "https://tecnetone.com/hubfs/Pentesting%20Cloud.webp";
+  imagen2 ="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQtKRD-hcbB5AuuNpdUoNFab4egq2Ubv2Mvu6WwELxN-3sMVTSPow-zQZDY2A8t";
+  mensajeColor: string = 'red';
+
+
+
+
 
   login() {
     const user = "admin";
